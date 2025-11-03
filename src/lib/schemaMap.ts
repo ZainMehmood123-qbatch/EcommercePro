3ca0ffeb59bf889946599e1ee8f728d1b4b42b4b
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-03 14:10:46 +0500

    add JOI validation in getOrders api

--- src/lib/schemaMap.ts
+++ src/lib/schemaMap.ts
@@ -1,15 +1,19 @@
 // schemaMap.ts
 import { getProductsSchema, productCreateSchema, productUpdateSchema, variantSchema } from '@/validations/productSchema';
 import { signupSchema, resetpasswordSchema} from '@/validations/authSchema';
-import { createOrderSchema } from '@/validations/orderSchema';
+import { createOrderSchema, getOrdersSchema } from '@/validations/orderSchema';
 
 export const validationMap = [
   { method: 'GET', path: '/api/products', schema: getProductsSchema },
   { method: 'POST', path: '/api/products', schema: productCreateSchema },
   { method: 'PUT', path: /^\/api\/products\/[^/]+$/, schema: productUpdateSchema },
+
   { method: 'POST', path: '/api/variants', schema: variantSchema },
   { method: 'PUT', path: /^\/api\/variants\/[^/]+$/, schema: variantSchema },
+
+  { method: 'GET', path: '/api/orders', schema: getOrdersSchema},
   { method: 'POST', path: '/api/checkout', schema: createOrderSchema },
+  
   { method: 'POST', path: '/api/signup', schema: signupSchema },
   { method: 'POST', path: '/api/reset-password', schema: resetpasswordSchema }
 ];
--- src/validations/orderSchema.ts
+++ src/validations/orderSchema.ts
@@ -14,6 +14,21 @@ export const orderItemSchema = Joi.object({
   stock: Joi.number().integer().min(0).optional()
 });
 
+export const getOrdersSchema = Joi.object({
+  page: Joi.number().integer().min(1).default(1).messages({
+    'number.base': 'Page must be a number',
+    'number.min': 'Page must be at least 1'
+  }),
+  limit: Joi.number().integer().min(1).max(100).default(10).messages({
+    'number.base': 'Limit must be a number',
+    'number.min': 'Limit must be at least 1',
+    'number.max': 'Limit cannot exceed 100'
+  }),
+  search: Joi.string().allow('').optional().messages({
+    'string.base': 'Search must be a string'
+  })
+});
+
 export const createOrderSchema = Joi.object({
   items: Joi.array().items(orderItemSchema).min(1).required(),
   tax: Joi.number().min(0).optional(),
