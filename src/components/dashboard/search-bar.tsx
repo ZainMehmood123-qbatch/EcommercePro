bab27bcf56137a288f6dac755d717d0a3478c910
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 12:04:56 +0500

    fixed alignment of Dropdown File

--- src/components/dashboard/search-bar.tsx
+++ src/components/dashboard/search-bar.tsx
@@ -1,7 +1,7 @@
- /* eslint-disable no-unused-vars */
 'use client';
 
 import React from 'react';
+
 import { Input } from 'antd';
 
 interface GenericSearchProps<T> {
@@ -11,25 +11,26 @@ interface GenericSearchProps<T> {
   className?: string;
 }
 
-function SearchComponent<T extends string | number>({
+const SearchComponent = <T extends string | number>({
   searchTerm,
   setSearchTerm,
   placeholder = 'Search...',
   className = ''
-}: GenericSearchProps<T>) {
+}: GenericSearchProps<T>) => {
   return (
     <div className={`w-full sm:max-w-md lg:max-w-lg xl:max-w-xl ${className}`}>
       <Input.Search
+        allowClear
+        // eslint-disable-next-line react/jsx-curly-brace-presence
+        className="!text-black border border-[#E2E8F0] rounded-lg
+          [&_.ant-input]:!bg-white 
+          [&_.ant-input-search-button_svg]:!text-black"
         placeholder={placeholder}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value as T)}
-        className='!text-black border border-[#E2E8F0] rounded-lg
-          [&_.ant-input]:!bg-white 
-          [&_.ant-input-search-button_svg]:!text-black'
-        allowClear
       />
     </div>
   );
-}
+};
 
 export default SearchComponent;
