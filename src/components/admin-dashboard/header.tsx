8aa16459ebce2810e557e8b6fba2e44651c3850f
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 12:24:54 +0500

    convert header component to arrow func

--- src/components/admin-dashboard/header.tsx
+++ src/components/admin-dashboard/header.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className='h-16 bg-white border-b border-[#E8E8EC] flex justify-end items-center px-6'>
-      <span className='text-sm font-medium text-[#007BFF]'>Super Admin</span>
+    <header
+      className={'h-16 bg-white border-b border-[#E8E8EC] flex justify-end items-center px-6'}
+    >
+      <span className={'text-sm font-medium text-[#007BFF]'}>Super Admin</span>
     </header>
   );
-}
+};
+
+export default Header;
