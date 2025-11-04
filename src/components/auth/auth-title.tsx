894307fedfc024daad2e53fee10b5934ba217c77
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 12:07:06 +0500

    convert authTitle component to arrow func

--- src/components/auth/auth-title.tsx
+++ src/components/auth/auth-title.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-export default function AuthTitle({ text }: { text: string }) {
-  return (
-    <h1 className='text-2xl font-semibold text-center text-[#007BFF] mb-6'>
-      {text}
-    </h1>
-  );
-}
+const AuthTitle = ({ text }: { text: string }) => (
+  <h1 className={'text-2xl font-semibold text-center text-[#007BFF] mb-6'}>{text}</h1>
+);
+
+export default AuthTitle;
