a290857a93c49c70d15a3ae44eb57296cd1c2c27
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 11:52:27 +0500

    fixed alignment of providers file

--- src/store/providers.tsx
+++ src/store/providers.tsx
@@ -2,9 +2,10 @@
 
 import React from 'react';
 
-import { store } from './index';
 import { Provider } from 'react-redux';
 
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
+import { store } from './index';
+
+export const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
-}
+};
