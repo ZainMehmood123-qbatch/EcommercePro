ec69b6337070e6f01df271b9f1e956f46c509fb6
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 11:51:55 +0500

    fixed alignment of index file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+
 import ordersReducer from './slice/orders-slice';
 import productsReducer from './slice/products-slice';
-
-import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
     orders: ordersReducer,
-     products: productsReducer
+    products: productsReducer
   }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+// eslint-disable-next-line import/order, import/named
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
