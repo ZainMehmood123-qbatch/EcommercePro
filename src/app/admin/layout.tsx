c9e686f1d01fa575e47e7fbb67ef540f045a59f4
ZainMehmood123-qbatch <zain.mehmood@qbatch.com>
2025-11-04 12:51:19 +0500

    adjust apis

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,19 +4,16 @@ import React from 'react';
 
 import Sidebar from '@/components/admin-dashboard/sidebar';
 import Header from '@/components/admin-dashboard/header';
-
 import './layout.css';
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='ad-layout-whole'>
-      <Sidebar />
-      <div className='ad-layout-first'>
-        <Header />
-        <main className='ad-layout-second'>
-          {children}
-        </main>
-      </div>
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className={'ad-layout-whole'}>
+    <Sidebar />
+    <div className={'ad-layout-first'}>
+      <Header />
+      <main className={'ad-layout-second'}>{children}</main>
     </div>
-  );
-}
+  </div>
+);
+
+export default DashboardLayout;
